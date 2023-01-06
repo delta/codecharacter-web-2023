2bb3a4918bc9a5673dd82f6647c05a3d22207bbd
shubham-1806 <shub.agarwal2003@gmail.com>
2023-01-06 19:36:35 +0530

    feat(Homepage,Commitpage) : Added commit history and home page designs

--- src/components/CommitHistory/CommitTree/CommitHistroy.tsx
+++ src/components/CommitHistory/CommitTree/CommitHistroy.tsx
@@ -1,6 +1,9 @@
 import { CodeRevision, GameMapRevision } from '@codecharacter-2023/client';
-import React, { useState } from 'react';
-import { VerticalTimelineElement } from 'react-vertical-timeline-component';
+import React, { useEffect, useState } from 'react';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import './CommitHistory.css';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
@@ -120,29 +123,29 @@ export default function CommitHistory(props: PropsType): JSX.Element {
     dispatch(isSelfMatchModalOpened(true));
   }
   const parseTimeFormat = (machineTime: string) => {
-    const commitTimestamp = new Date(machineTime);
-    const datePart = commitTimestamp.toDateString().substring(4, 10);
-    const timePart = commitTimestamp.toLocaleTimeString();
-    return `${datePart} at ${timePart}`;
+    const datepart = machineTime.substring(8, 10);
+    const monthpart = machineTime.substring(5, 7);
+    const yearpart = machineTime.substring(0, 4);
+    return `${datepart}-${monthpart}-${yearpart}`;
   };
 
   return (
-    <div>
+    <VerticalTimeline layout="1-column-left" lineColor="#EAEAEA">
       {props.commitHistoryDetails && props.commitHistoryDetails.length > 0 ? (
         props.commitHistoryDetails.map(eachCommit => {
           return (
             <VerticalTimelineElement
               key={eachCommit.id.toString()}
               className="vertical-timeline-element--work"
-              contentStyle={{ background: '#242a3c', color: '#fff' }}
-              contentArrowStyle={{
-                borderRight: '7px solid  rgb(33, 150, 243)',
+              contentStyle={{
+                background: 'rgba(26, 26, 26, 0.7)',
+                color: '#fff',
               }}
               date={parseTimeFormat(eachCommit.createdAt.toString())}
               iconStyle={
                 commitNumber == eachCommit.id
                   ? CircleIcon
-                  : { background: 'rgb(33, 150, 243)', color: '#fff' }
+                  : { background: '#444444', color: '#444444' }
               }
               onTimelineElementClick={() => {
                 setCommitNumber(eachCommit.id);
@@ -152,25 +155,17 @@ export default function CommitHistory(props: PropsType): JSX.Element {
               <h6 className="vertical-timeline-element-title">
                 {eachCommit.message}
               </h6>
-              <div
+              {/* <div
                 className="flex d-flex justify-content-start"
                 onClick={e => handleCommitSelect(e)}
               >
-                <Button
-                  className={styles.selectBtn}
-                  data-name={eachCommit.message}
-                  data-uuid={eachCommit.id}
-                >
-                  {' '}
-                  <FontAwesomeIcon icon={faPlay as IconProp} /> <b>Simulate</b>{' '}
-                </Button>
-              </div>
+              </div> */}
             </VerticalTimelineElement>
           );
         })
       ) : (
         <h1 className="noCommitStyle">No Commits available</h1>
       )}
-    </div>
+    </VerticalTimeline>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,40 @@
 import Footer from '../../components/Home/Footer/Footer';
 import Glitchtext from '../../components/Home/Glitchtext/Glitchtext';
+import Particle from '../../components/Home/Particles/Particle';
 import Rows from '../../components/Home/Row/Rows';
 import styles from './Home.module.css';
+import { useState } from 'react';
+import MatrixBackground from '../../components/MatrixEffect/Matrix';
 
 export default function Home(): JSX.Element {
+  const [pageNum, setPageNum] = useState<Number>(1);
   return (
     <main className={styles.mainContainer}>
-      <div className={styles.jumbotron}>
-        <Glitchtext value="Code Character" />
-      </div>
-      <Rows />
-      <Footer />
-      <div className={styles.delta}>
-        <div>
-          Made with <span className={styles.heart}>❤</span> by{' '}
-          <a href="https://delta.nitt.edu/">Delta Force</a>
+      {pageNum == 1 && (
+        <div className={styles.jumbotron}>
+          <MatrixBackground />
+          <Glitchtext value="Code Character" />
+          <img
+            onClick={() => {
+              setPageNum(1);
+            }}
+            className={styles.arrow}
+            src="/assets/UnderlineHeading.svg"
+          ></img>
         </div>
-      </div>
+      )}
+      {pageNum == 1 && (
+        <>
+          <Rows />
+          <Footer />
+          <div className={styles.delta}>
+            <div>
+              Made with <span className={styles.heart}>❤</span> by{' '}
+              <a href="https://delta.nitt.edu/">Delta Force</a>
+            </div>
+          </div>
+        </>
+      )}
     </main>
   );
 }
