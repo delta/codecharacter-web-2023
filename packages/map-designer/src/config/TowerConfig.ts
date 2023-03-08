d666119af459d3874f212c918d74169589d7b7b6
b1otch <vvivek1801@gmail.com>
2023-03-08 20:15:58 +0530

    update(towers): Naming the towers

--- packages/map-designer/src/config/TowerConfig.ts
+++ packages/map-designer/src/config/TowerConfig.ts
@@ -3,8 +3,8 @@ import TowerType from './TowerType.js';
 export default class TowerConfig {
   // TODO: Fetch from network
   static readonly towers = [
-    new TowerType(2, 'tower1', 'tower1.png', 'tower1_thumbnail.png', 100),
-    new TowerType(3, 'tower aerial', 'tower2.png', 'tower2_thumbnail.png', 200),
-    new TowerType(1, 'tower3', 'tower3.png', 'tower3_thumbnail.png', 200),
+    new TowerType(2, 'Ranger', 'tower1.png', 'tower1_thumbnail.png', 100),
+    new TowerType(3, 'Aerial', 'tower2.png', 'tower2_thumbnail.png', 200),
+    new TowerType(1, 'Common', 'tower3.png', 'tower3_thumbnail.png', 200),
   ];
 }
