8756a3e9b22b62b362e143e55d8b293ec0382eee
b1otch <vvivek1801@gmail.com>
2023-03-09 21:10:39 +0530

    fix: updated attacker values

--- packages/renderer/src/config/TroopConfig.ts
+++ packages/renderer/src/config/TroopConfig.ts
@@ -4,6 +4,6 @@ export default class TroopConfig {
   static readonly troops: Array<TroopType> = [
     new TroopType(10, 2, 'punk'),
     new TroopType(20, 4, 'cyborg'),
-    new TroopType(10, 6, 'drone'),
+    new TroopType(15, 6, 'drone'),
   ];
 }
