9c80af5fc24513092f5a92b4ea2dfbb7a5645765
b1otch <vvivek1801@gmail.com>
2023-03-09 18:25:19 +0530

    fix(defender assets): fixed tower mismatch with assets

--- packages/map-designer/src/config/TowerConfig.ts
+++ packages/map-designer/src/config/TowerConfig.ts
@@ -3,8 +3,8 @@ import TowerType from './TowerType.js';
 export default class TowerConfig {
   // TODO: Fetch from network
   static readonly towers = [
-    new TowerType(2, 'tower1', 'tower1.png', 'tower1_thumbnail.png', 100),
-    new TowerType(3, 'tower aerial', 'tower2.png', 'tower2_thumbnail.png', 200),
-    new TowerType(1, 'tower3', 'tower3.png', 'tower3_thumbnail.png', 200),
+    new TowerType(0, 'Common', 'tower1.png', 'tower1_thumbnail.png', 25),
+    new TowerType(1, 'Ranger', 'tower2.png', 'tower2_thumbnail.png', 50),
+    new TowerType(2, 'Aerial', 'tower3.png', 'tower3_thumbnail.png', 100),
   ];
 }
